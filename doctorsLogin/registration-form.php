b6f76955e5fd6b93fa5bdfc6f8a2e5d10cee1806
Christian Buenaflor <wengv200@gmail.com>
2025-05-21 05:52:58 +0800

    Update registration-form.php
    UPDATE PLEASE

--- doctorsLogin/registration-form.php
+++ doctorsLogin/registration-form.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 include("includes/header.php");
 include_once("connection/globalConnection.php");
 
 $con = connection();
 
-if(isset($_POST['submit'])){
-    
+if (isset($_POST['submit'])) {
+
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
     $email = $_POST['emailaddress'];
@@ -13,42 +13,46 @@ if(isset($_POST['submit'])){
     $dob = $_POST['dob'];
     $password = $_POST['password'];
     $username = $_POST['username'];
-    $usertype ="Patient";
+    $usertype = "Patient";
 
 
-    try{
-        
-            $fname = $con->real_escape_string($firstname);
-            $lastname = $con->real_escape_string($lastname);
-            $email = $con->real_escape_string($email);
-            $phone_number = $con->real_escape_string($phone_number);
-            $dob = $con->real_escape_string($dob);
-            $username = $con->real_escape_string($username);
-            $password = $con->real_escape_string($password);
-            
-            
-            $sql_login_patient = "INSERT INTO userlogintb (User_Name, Password, User_Type) VALUES ('$username', '$password', '$usertype')";
+        $fname = $con->real_escape_string($firstname);
+        $lastname = $con->real_escape_string($lastname);
+        $email = $con->real_escape_string($email);
+        $phone_number = $con->real_escape_string($phone_number);
+        $dob = $con->real_escape_string($dob);
+        $username = $con->real_escape_string($username);
+        $password = $con->real_escape_string($password);
 
-           
 
-        if($con->query($sql_login_patient) === TRUE){
-            
+        $sql_login_patient = "INSERT INTO userlogintb (User_Name, Password, User_Type) VALUES ('$username', '$password', '$usertype')";
+
+        if ($con->query($sql_login_patient) === TRUE) {
+
             $get_id_from_patient = $con->insert_id;
 
-            $sql_insert_patient = "INSERT INTO patienttb (First_Name, Last_Name, Email_address, Phone_Number, Date_Birth) VALUES 
-                        ('$fname', '$lastname', $email, '$phone_number', '$dob')";
+            $sql_insert_patient = "INSERT INTO patienttb (patient_acc_id, First_Name, Last_Name, Email_address, Phone_Number, Date_Birth) VALUES 
+                        ('$get_id_from_patient', '$fname', '$lastname', '$email', '$phone_number', '$dob')";
+
+            if ($con->query($sql_insert_patient) === TRUE) {
+                $con->commit();
+                $showToast = true;
+                $toastMessage = "Successfully Added!";
+                $isSuccess = true;
+            } else {
+                $showToast = true;
+                $toastMessage = "Error updating doctor: " . $con->error;
+                $isSuccess = false;
+            }
         }
     }
-    catch (\Throwable $e) {
-        # code...
-    }
-}
 
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -167,7 +171,7 @@ if(isset($_POST['submit'])){
             border: 2px solid #22a6ce;
         }
 
-        
+
 
         .form-label {
             font-size: 14px;
@@ -198,7 +202,7 @@ if(isset($_POST['submit'])){
                 min-height: auto;
                 padding: 30px 20px;
             }
-            
+
             .registration-container {
                 margin: 20px;
             }
@@ -216,6 +220,7 @@ if(isset($_POST['submit'])){
         }
     </style>
 </head>
+
 <body>
     <div class="wave-bg"></div>
     <div class="container">
@@ -227,45 +232,45 @@ if(isset($_POST['submit'])){
                         <div class="col-md-5 left-panel">
                             <a href="login.php" class="back-btn mb-4">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
-                                    <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+                                    <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
                                 </svg>
                                 <span class="ms-2">Back</span>
                             </a>
-                            
+
                             <h1>Expert advice from top doctors</h1>
-                            
+
                             <div class="feature-item">
                                 <div class="feature-icon">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
-                                        <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                                        <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
                                     </svg>
                                 </div>
                                 <div>Expert advice from top doctors</div>
                             </div>
-                            
+
                             <div class="feature-item">
                                 <div class="feature-icon">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
-                                        <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
-                                        <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>
+                                        <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
+                                        <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
                                     </svg>
                                 </div>
                                 <div>Available 24/7 on any device</div>
                             </div>
-                            
+
                             <div class="feature-item">
                                 <div class="feature-icon">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-shield-check" viewBox="0 0 16 16">
-                                        <path d="M5.338 1.59a61.44 61.44 0 0 0-2.837.856.481.481 0 0 0-.328.39c-.554 4.157.726 7.19 2.253 9.188a10.725 10.725 0 0 0 2.287 2.233c.346.244.652.42.893.533.12.057.218.095.293.118a.55.55 0 0 0 .101.025.615.615 0 0 0 .1-.025c.076-.023.174-.061.294-.118.24-.113.547-.29.893-.533a10.726 10.726 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067c-.53 0-1.552.223-2.662.524zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.775 11.775 0 0 1-2.517 2.453 7.159 7.159 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7.158 7.158 0 0 1-1.048-.625 11.777 11.777 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 62.456 62.456 0 0 1 5.072.56z"/>
-                                        <path d="M10.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+                                        <path d="M5.338 1.59a61.44 61.44 0 0 0-2.837.856.481.481 0 0 0-.328.39c-.554 4.157.726 7.19 2.253 9.188a10.725 10.725 0 0 0 2.287 2.233c.346.244.652.42.893.533.12.057.218.095.293.118a.55.55 0 0 0 .101.025.615.615 0 0 0 .1-.025c.076-.023.174-.061.294-.118.24-.113.547-.29.893-.533a10.726 10.726 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067c-.53 0-1.552.223-2.662.524zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.775 11.775 0 0 1-2.517 2.453 7.159 7.159 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7.158 7.158 0 0 1-1.048-.625 11.777 11.777 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 62.456 62.456 0 0 1 5.072.56z" />
+                                        <path d="M10.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
                                     </svg>
                                 </div>
                                 <div>Private questions answered within 24 hrs</div>
                             </div>
-                            
+
                             <div class="wave-accent"></div>
                         </div>
-                        
+
                         <!-- Right Panel -->
                         <div class="col-md-7 right-panel">
                             <!-- <div class="user-type">
@@ -287,10 +292,10 @@ if(isset($_POST['submit'])){
                                     <div>Doctor</div>
                                 </div>
                             </div> -->
-                            
+
                             <h4 class="mb-4 text-center">Create your account</h4>
                             <p class="text-center text-muted mb-4">Sign up to connect with our network of medical professionals.</p>
-                            
+
                             <form action="" method="post">
                                 <div class="row mb-3">
                                     <div class="col-md-6 mb-3 mb-md-0">
@@ -302,57 +307,57 @@ if(isset($_POST['submit'])){
                                         <input type="text" name="lastname" class="form-control" id="lastName" placeholder="Last Name" required>
                                     </div>
                                 </div>
-                                
+
                                 <div class="mb-3">
                                     <label for="email" class="form-label">Email Address</label>
                                     <input type="email" name="emailaddress" class="form-control" id="email" placeholder="Email Address" required>
                                     <div class="text-muted mt-1" style="font-size: 12px;">We'll never share your email with anyone else</div>
                                 </div>
-                                
+
                                 <div class="mb-3">
                                     <label for="phone" class="form-label">Phone Number</label>
                                     <input type="tel" name="phonenumber" class="form-control" id="phone" placeholder="Phone Number">
                                 </div>
-                                
+
                                 <div class="mb-3">
                                     <label for="dob" class="form-label">Date of Birth</label>
                                     <input type="date" name="dob" class="form-control" id="dob" required>
                                 </div>
 
-                                 <div class="mb-3">
+                                <div class="mb-3">
                                     <label for="phone" class="form-label">Username</label>
                                     <input type="text" name="username" class="form-control" id="username" placeholder="Username">
                                 </div>
-                                
+
                                 <div class="mb-3">
                                     <label for="password" class="form-label">Create Password</label>
                                     <div class="input-group">
                                         <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
                                         <span class="input-group-text">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
-                                                <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
-                                                <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+                                                <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                                                <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
                                             </svg>
                                         </span>
                                     </div>
                                     <div class="text-muted mt-1" style="font-size: 12px;">Password must be at least 8 characters</div>
                                 </div>
-                                
+
                                 <div class="mb-3">
                                     <label for="confirmPassword" class="form-label">Confirm Password</label>
                                     <input type="password" class="form-control" id="confirmPassword" placeholder="Confirm Password" required>
                                 </div>
-                                
+
                                 <div class="mb-3 form-check">
                                     <input type="checkbox" class="form-check-input" id="termsCheck" required>
                                     <label class="form-check-label" for="termsCheck">I agree to the <a href="#" class="login-link">Terms of Service</a> and <a href="#" class="login-link">Privacy Policy</a></label>
                                 </div>
-                                
+
                                 <div class="mb-3 mt-4">
                                     <input type="submit" class="btn sign-up-btn" name="submit" value="Create Account">
                                 </div>
                             </form>
-                            
+
                             <div class="text-center mt-4">
                                 Already have an account? <a href="login.php" class="login-link">Sign in</a>
                             </div>
@@ -362,7 +367,7 @@ if(isset($_POST['submit'])){
             </div>
         </div>
     </div>
-    
+
     <!-- Bootstrap JS Bundle -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
     <script>
@@ -373,12 +378,12 @@ if(isset($_POST['submit'])){
                 this.classList.add('active');
             });
         });
-        
+
         // Password visibility toggle
         document.querySelector('.input-group-text').addEventListener('click', function() {
             const passwordInput = document.getElementById('password');
             const icon = this.querySelector('svg');
-            
+
             if (passwordInput.type === 'password') {
                 passwordInput.type = 'text';
                 icon.innerHTML = '<path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z"/><path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z"/><path d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z"/>';
@@ -387,18 +392,18 @@ if(isset($_POST['submit'])){
                 icon.innerHTML = '<path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/><path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>';
             }
         });
-        
+
         // Form validation
         const form = document.querySelector('form');
         form.addEventListener('submit', function(event) {
             const password = document.getElementById('password');
             const confirmPassword = document.getElementById('confirmPassword');
-            
+
             if (password.value !== confirmPassword.value) {
                 event.preventDefault();
                 alert('Passwords do not match!');
             }
-            
+
             if (password.value.length < 8) {
                 event.preventDefault();
                 alert('Password must be at least 8 characters long!');
@@ -406,4 +411,5 @@ if(isset($_POST['submit'])){
         });
     </script>
 </body>
+
 </html>
